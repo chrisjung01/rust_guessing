5e63d35126d416b381ce53ee3638294394102907
Christian Jung <chris.j88@t-online.de>
2025-06-09 09:33:42 +0200

    Add a secret numer between 1-100 and log this in debug mode

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 use std::io;
 
+use rand::Rng;
+
 fn main() {
+    let secret_number = rand::rng().random_range(1..100);
+
+    // The secret number is only printed in debug mode
+    if cfg!(debug_assertions) {
+        println!("Debug mode is enabled");
+        println!("The secret number is: {}", secret_number);
+    }
+
     println!("Guess the number game!");
 
     println!("Please enter your guess:");
